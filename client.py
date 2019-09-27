3502f308f94e5e4f970d5d9dae1a1f1ebb9c60dc
Bertrand Wiart <bertrand.wiart@reseau.Eseo.fr>
2019-09-27 11:33:46 +0200

    test new manifest

--- client.py
+++ client.py
@@ -1,10 +1,66 @@
+#!/usr/bin/env python3
 
-#!/usr/bin/env python
+from settings import *
+device.log(message='settings imported !', message_type='success')
 
-'''Hello Farmware
-A simple Farmware example that tells FarmBot to log a new message.
-'''
+import socket
+device.log(message='import socket !', message_type='success')
 
-from farmware_tools import device
+from time import sleep
+device.log(message='import time !', message_type='success')
 
-device.log(message='Hello Farmware!', message_type='success')
+device.log(message='starting Client !', message_type='success')
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def connect(host,port,retry=1):
+    device.log(message='before connect', message_type='success')
+    connected = False
+    try:
+        s.connect((host, port))
+        device.log(message='connected !', message_type='success')
+        connected = True
+    except ConnectionRefusedError:
+        device.log(message='connection refused !', message_type='success')
+        #print("Connection refused")
+        if retry<5:
+            #print("Retrying (try n°{})".format(retry+1))
+            sleep(1)
+            connected = connect(host, port,retry+1)
+            retry+=1
+        else:
+            #print("Le serveur distant n'est pas joignable.")
+        	device.log(message='server non joignable !', message_type='success')
+    finally:return connected
+
+if __name__ == "__main__":
+    run = True
+    connected = connect(IP, PORT)
+
+    #print("----"*5)
+
+    if connected:
+        while run:
+            
+            # send a rq_connect command
+            sendMsg(s, 1, PASS)
+                
+            # receive the response
+            run, dataBytes, order, size = receiveMsg(s)
+
+            # display on the web app if connectSuccess is received
+            if (order == 'rsp_connectSuccess'):
+                #print("\nAction performed on the web App :")
+                device.log(message='Connected with to the server !', message_type='success')
+            else:
+                #print("Received connection failed flag, Good Bye")
+                device.log(message='Received connection failed flag ! ', message_type='success')
+            # close the socket
+            #print("\nProcess is now done, Good bye !")
+            run = False
+            s.close()
+
+    #print("----"*5)
+
+
---
+++ settings.py
@@ -0,0 +1,112 @@
+from farmware_tools import device
+
+commands = {
+    'rq_connect' : 1,
+    'rsp_connectSuccess' : 2,
+    'rsp_connectFail' : 3
+}
+
+#IP = "localhost"
+IP = "172.14.1.23"
+PORT = 36100
+PASS = "farmbot"
+
+
+def sendMsg(socket, order, msg="", encoding="utf-8"):
+    device.log(message='Setting sending message !', message_type='success')
+    #print("==== SENDING MSG ====")
+    #print("Sending {} message".format(getDictKey(order)))
+    size = len(msg)
+    byte_size = size.to_bytes(3, byteorder="big", signed=False)
+    header = bytearray(4)
+    header[0] = order
+    for i in range(3):
+        header[i+1] = byte_size[i]
+
+    if encoding == "bytes":
+        msgBytes = msg
+    else:
+        msgBytes = bytearray(msg, encoding)
+
+    sendData = header + msgBytes
+    #print("Message sent : {}".format(sendData))
+    #print("=====================")
+
+    socket.send(sendData)
+
+def getDictKey(value):
+    for key, val in commands.items():
+        if val == value:
+            return key
+    return None
+
+
+def receiveHeader(socket):
+    try :
+        headerBytes = bytearray(socket.recv(4))
+        #print("Header : {}".format(headerBytes))
+        device.log(message='Setting receiveHEader !', message_type='success')
+
+        order, size = None, None
+        if len(headerBytes) > 0:
+            order = getDictKey(headerBytes[0])
+            #print("::::::::::\nSIZE : {}".format(headerBytes[1:]))
+            headerBytes[0] = 0
+            size = int.from_bytes(headerBytes, byteorder='big')
+            #print("Order : {}".format(order))
+            #print("Size  : {}\n::::::::::".format(size))
+    except ConnectionResetError:
+        #print("Connection reset by peer")
+        order = 0
+        size = 0
+
+
+    return order, size
+
+
+def receiveMsg(socket, side="client"):
+    #print("==== RECEIVING MSG ====")
+    device.log(message='Settings ReceiveMEssage !', message_type='success')
+    order, size = receiveHeader(socket)
+    if order == None:
+        #print("[ERROR] Order = None")
+        return False, None
+
+
+    dataBytes = bytearray()
+
+    if (size>0):
+        dataBytes = socket.recv(size)
+
+    if side == "server":
+        if order == "rq_connect":
+            #print("Password received : {}".format(dataBytes.decode()))
+            if (dataBytes.decode() == PASS):
+                #print("Good password !")
+                sendMsg(socket, commands["rsp_connectSuccess"])
+                return True, None
+            else:
+                #print("Bad password !")
+                sendMsg(socket, commands["rsp_connectFail"])
+                return False, None
+
+        else:
+            #print("[ERROR] Received a message destinated to a client, but i'm a server")
+            return False, None
+
+    else:
+
+        if order == "rsp_connectSuccess":
+            #print("Connection success !")
+            return True, None, order, size
+
+        elif order == "rsp_connectFail":
+            #print("Connection failed !")
+            return False, None, order, size
+
+        else:
+            #print("[ERROR] Received a message destinated to a server, but i'm a client")
+            return False, None, order, size
+    
+    #print("=======================")
+
